a0f3f66fb8433e29ee3995a63fd4c506d35a10f7
Kartikey Mishra <mkartikey666@gmail.com>
2023-05-12 07:46:46 +0530

    new questions started

---
+++ deletion-LL.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node()
+    {
+        this->value = -1;
+        this->next = NULL;
+    }
+};
